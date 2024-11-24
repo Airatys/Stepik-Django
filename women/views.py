3b403f7b3423d5c31a721dc4dc1ebe15e1d4e24f
Airat Sharifullin <sharifullin.airat@yandex.ru>
2024-11-24 11:52:41 +0300

    stepik

--- women/views.py
+++ women/views.py
@@ -3,11 +3,16 @@ from contextlib import redirect_stderr
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Страница приложения women.')
+    t = render_to_string('women/index.html')
+    return HttpResponse(t)
+
+def about(request):
+    return render(request, 'women/about.html')
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Статьи по категориям</h1><p >id:{cat_id}</p>')
