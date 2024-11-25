6d1e4131f8864b563b29fc0166f11e00532e9687
Airat Sharifullin <sharifullin.airat@yandex.ru>
2024-11-25 23:18:46 +0300

    stepik

--- women/views.py
+++ women/views.py
@@ -4,15 +4,33 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
 
 
 # Create your views here.
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
 def index(request):
-    t = render_to_string('women/index.html')
+    data = {
+        'title': 'главная страница?',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+        'url': slugify("The main page"),
+    }
+    t = render_to_string('women/index.html', context=data)
     return HttpResponse(t)
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Статьи по категориям</h1><p >id:{cat_id}</p>')
